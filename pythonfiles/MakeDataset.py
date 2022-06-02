6a2858646b5a8848bb3475e6da4abc978f69484d
irohasukensirou <kensirousuizu@gmail.com>
2022-06-02 18:04:49 +0900

    データセット作成

--- pythonfiles/MakeDataset.py
+++ pythonfiles/MakeDataset.py
@@ -1,73 +1,50 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Sun May 29 12:16:14 2022
-
-@author: kaito
-"""
-
-import os
+import torch
+from torch.utils.data import Dataset, DataLoader
 import cv2
-import sys
 import numpy as np
-import pandas as pd
-import matplotlib.pyplot as plt
-from matplotlib.patches import Rectangle
 from glob import glob
-from tqdm import tqdm
-
-df = pd.read_csv("../input/train.csv") #read train data
-df = df.sort_values(["id", "class"]).reset_index(drop = True) #id:case_day_slice
-df["patient"] = df.id.apply(lambda x: x.split("_")[0]) # case
-df["days"] = df.id.apply(lambda x: "_".join(x.split("_")[:2])) #day
-
-all_image_files = sorted(glob('../input/train/*/*/scans/*.png'),key = lambda x: x.split("/")[1] + "_" + x.split("/")[2])
-#slice[0]_slicenum[1]_sizex[2]_sizey[3]_spacingx[4]_spacingy[5]        ex.slice_0001_266_266_1.50_1.50
-size_x = [int(os.path.basename(_)[:-4].split("_")[-4]) for _ in all_image_files] #[:-4] -> remove ".png"
-size_y = [int(os.path.basename(_)[:-4].split("_")[-3]) for _ in all_image_files]
-spacing_x = [float(os.path.basename(_)[:-4].split("_")[-2]) for _ in all_image_files]
-spacing_y = [float(os.path.basename(_)[:-4].split("_")[-1]) for _ in all_image_files]
-df["image_files"] = np.repeat(all_image_files, 3) #3->"stomach" "large_bowel" "small_bowel"
-df["spacing_x"] = np.repeat(spacing_x, 3)
-df["spacing_y"] = np.repeat(spacing_y, 3)
-df["size_x"] = np.repeat(size_x, 3)
-df["size_y"] = np.repeat(size_y, 3)
-df["slice"] = np.repeat([int(os.path.basename(_)[:-4].split("_")[-5]) for _ in all_image_files], 3) #slice
-
-def rle_decode(mask_rle, shape):
-    s = np.array(mask_rle.split(), dtype=int)
-    starts, lengths = s[0::2] - 1, s[1::2] 
-    ends = starts + lengths
-    h, w = shape
-    img = np.zeros((h * w,), dtype = np.uint8) #flatten
-    for lo, hi in zip(starts, ends): #start-1 + length = end (length include start)
-        img[lo : hi] = 1
-    return img.reshape(shape)
+import torchvision.transforms as transforms
 
 
-for day, group in tqdm(df.groupby("days")): #144 scans per day -> imgs,msks
-    patient = group.patient.iloc[0]
-    imgs,msks,file_names = [],[],[]
-    for file_name in group.image_files.unique(): #1group -> "stomach" "large_bowel" "small_bowel"(3labels)
-        img = cv2.imread(file_name, cv2.IMREAD_ANYDEPTH) #(266,266) ...fluctuate xy size but size are mostly it
-        segms = group.loc[group.image_files == file_name] 
-        masks = {} #3label mask
-        for segm, label in zip(segms.segmentation, segms['class']): #1lebel + 1segm -> 1mask
-            if not pd.isna(segm):
-                mask = rle_decode(segm,[segms.size_x.iloc[0], segms.size_y.iloc[0]])
-                masks[label] = mask
-            else:
-                masks[label] = np.zeros((segms.size_x.iloc[0], segms.size_y.iloc[0]), dtype = np.uint8)
-        masks = np.stack([masks[k] for k in sorted(masks)], -1)
-        imgs.append(img) #(1,266,266)
-        msks.append(masks) #(1,266,266,3)
+class BuildDataset(Dataset):
+    def __init__(self, image_paths, mask_paths, size=(256, 256), label=True, transforms=None):
+        self.img_paths  = sorted(glob(f'{image_paths}/*.png'), key = lambda x: x.split('\\')[1].split('_')[1])
+        self.mask_paths = sorted(glob(f'{mask_paths}/*.png'), key = lambda x: x.split('\\')[1].split('_')[1])
+        self.size = size
+        self.label = label
         
-    imgs = np.stack(imgs, 0) #(144,266,266) ...fluctuate xy size
-    msks = np.stack(msks, 0) #(144,266,266,3) ...fluctuate xy size
+    def __len__(self):
+        return len(self.img_paths)
     
-    for i in range(msks.shape[0]):
-        img = imgs[[max(0, i - 2), i, min(imgs.shape[0] - 1, i + 2)]].transpose(1,2,0)    
-        msk = msks[i]
+    def __getitem__(self, index):
+        img_path  = self.img_paths[index]
+        img = cv2.imread(img_path, cv2.IMREAD_UNCHANGED).astype('float32')
+        img = cv2.resize(img, dsize=self.size)
+        img /= img.max(axis=(0,1))
+        
+        if self.label:
+            mask_path = self.mask_paths[index]
+            mask = cv2.imread(mask_path, cv2.IMREAD_UNCHANGED).astype('float32')
+            mask = cv2.resize(mask, dsize=self.size)
+            mask /= 255.0
+#             if self.transforms:
+#                 data = self.transforms(image=img, mask=msk)
+#                 img  = data['image']
+#                 msk  = data['mask']
+            img = np.transpose(img, (2, 0, 1))  #(3, x, y)
+            mask = np.transpose(mask, (2, 0, 1))  #(3, x, y)
+            return torch.tensor(img), torch.tensor(mask)
+        else:
+#             if self.transforms:
+#                 data = self.transforms(image=img)
+#                 img  = data['image']
+            img = np.transpose(img, (2, 0, 1))
+            return torch.tensor(img)
 
-        new_file_name = f"{day}_{i}.png"
-        cv2.imwrite(f"../input/seg_train/images/{new_file_name}", img)
-        cv2.imwrite(f"../input/seg_train/masks/{new_file_name}", msk)
+# image_paths = '../input/seg_train/images'
+# mask_paths = '../input/seg_train/masks'
+# train_dataset = BuildDataset(image_paths, mask_paths)
+# train_loader = DataLoader(train_dataset, batch_size=2, pin_memory=True)
+
+# imgs, msks = next(iter(train_loader))
+# print(imgs.size(), msks.size())
